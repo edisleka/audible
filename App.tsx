4ebddf0f94b2ce7c9daacf52e6613292d2e3a4aa
Edis Leka <edisleka@yahoo.gr>
2025-04-12 15:26:37 +0300

    Book List Item created

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import './global.css'
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { View } from 'react-native'
+import books from './src/books'
+import BookListItem from './src/components/BookListItem'
 
 export default function App() {
   return (
-    <View className='flex-1 items-center justify-center bg-slate-800'>
-      <Text className='text-2xl text-gray-200 font-bold'>NativeWind</Text>
+    <View className='flex-1 justify-center bg-slate-800 p-4'>
+      <BookListItem book={books[0]} />
+      <BookListItem book={books[1]} />
+
       <StatusBar style='auto' />
     </View>
   )
---
+++ src/components/BookListItem.tsx
@@ -0,0 +1,30 @@
+import { View, Image, Text } from 'react-native'
+import { AntDesign } from '@expo/vector-icons'
+
+interface Book {
+  id: string
+  title: string
+  author: string
+  thumbnail_url?: string
+  audio_url: string
+}
+
+interface BookListItemProps {
+  book: Book
+}
+
+export default function BookListItem({ book }: BookListItemProps) {
+  return (
+    <View className='flex-row gap-4 items-center'>
+      <Image
+        source={{ uri: book.thumbnail_url }}
+        className='w-16 aspect-square rounded-md'
+      />
+      <View className='gap-1 flex-1'>
+        <Text className='text-2xl text-gray-200 font-bold'>{book.title}</Text>
+        <Text className='text-gray-400'>{book.author}</Text>
+      </View>
+      <AntDesign name='playcircleo' size={24} color='gainsboro' />
+    </View>
+  )
+}
