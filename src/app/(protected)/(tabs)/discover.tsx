44174233d8678d2cd5a59e9ef0fb50e7e3a1991c
Edis Leka <edisleka@yahoo.gr>
2025-04-16 17:15:03 +0300

    Query Supabase using Tanstack Query

--- src/app/(protected)/(tabs)/discover.tsx
+++ src/app/(protected)/(tabs)/discover.tsx
@@ -1,8 +1,23 @@
-import { View, Text } from 'react-native'
+import { Text, ActivityIndicator, FlatList } from 'react-native'
+import { supabase } from '@/lib/supabase'
+import { useQuery } from '@tanstack/react-query'
+import BookListItem from '@/components/BookListItem'
+
 export default function DiscoverScreen() {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['books'],
+    queryFn: () => supabase.from('books').select('*').throwOnError(),
+  })
+
+  if (isLoading) return <ActivityIndicator />
+
+  if (error) return <Text>Error: {error.message}</Text>
+
   return (
-    <View className='flex-1 justify-center items-center'>
-      <Text>DiscoverScreen</Text>
-    </View>
+    <FlatList
+      data={data?.data || []}
+      contentContainerClassName='gap-4 bg-green-500'
+      renderItem={({ item }) => <BookListItem book={item} />}
+    />
   )
 }
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,6 +5,9 @@ import { DarkTheme, ThemeProvider } from '@react-navigation/native'
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import { StatusBar } from 'expo-status-bar'
 import PlayerProvider from '@/providers/PlayerProvider'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 const theme = {
   ...DarkTheme,
@@ -20,11 +23,13 @@ export default function RootLayout() {
   return (
     <>
       <ClerkProvider tokenCache={tokenCache}>
-        <ThemeProvider value={theme}>
-          <PlayerProvider>
-            <Slot />
-          </PlayerProvider>
-        </ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider value={theme}>
+            <PlayerProvider>
+              <Slot />
+            </PlayerProvider>
+          </ThemeProvider>
+        </QueryClientProvider>
       </ClerkProvider>
       <StatusBar style='light' />
     </>
