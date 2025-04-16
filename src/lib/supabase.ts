6ce801f6354cf71092d1f61af36fc6309e6faf2d
Edis Leka <edisleka@yahoo.gr>
2025-04-16 17:37:50 +0300

    Connect Clerk with Supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,13 @@
+import { useAuth } from '@clerk/clerk-expo'
 import { createClient } from '@supabase/supabase-js'
-import 'react-native-url-polyfill/auto'
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {})
+export const useSupabase = () => {
+  const { getToken } = useAuth()
+
+  return createClient(supabaseUrl, supabaseAnonKey, {
+    accessToken: async () => getToken() ?? null,
+  })
+}
