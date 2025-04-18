9c22a1ced0a84901d4c37e47e89fc94b288f5ea5
Edis Leka <edisleka@yahoo.gr>
2025-04-18 15:41:01 +0300

    user Library

--- src/app/(protected)/(tabs)/discover.tsx
+++ src/app/(protected)/(tabs)/discover.tsx
@@ -1,14 +1,14 @@
 import { Text, ActivityIndicator, FlatList } from 'react-native'
 import { useSupabase } from '@/lib/supabase'
 import { useQuery } from '@tanstack/react-query'
-import BookListItem from '@/components/BookListItem'
+import DiscoveryBookListItem from '@/components/DiscoveryBookListItem'
 
 export default function DiscoverScreen() {
   const supabase = useSupabase()
 
   const { data, error, isLoading } = useQuery({
     queryKey: ['books'],
-    queryFn: () => supabase.from('books').select('*').throwOnError(),
+    queryFn: async () => supabase.from('books').select('*').throwOnError(),
   })
 
   if (isLoading) return <ActivityIndicator />
@@ -18,8 +18,8 @@ export default function DiscoverScreen() {
   return (
     <FlatList
       data={data?.data || []}
-      contentContainerClassName='gap-4 bg-green-500'
-      renderItem={({ item }) => <BookListItem book={item} />}
+      contentContainerClassName='gap-4'
+      renderItem={({ item }) => <DiscoveryBookListItem book={item} />}
     />
   )
 }
--- src/app/(protected)/(tabs)/index.tsx
+++ src/app/(protected)/(tabs)/index.tsx
@@ -1,15 +1,31 @@
 import { StatusBar } from 'expo-status-bar'
-import { FlatList } from 'react-native'
+import { FlatList, ActivityIndicator, Text } from 'react-native'
 import BookListItem from '@/components/BookListItem'
 import books from '@/books'
+import { useQuery } from '@tanstack/react-query'
+import { useSupabase } from '@/lib/supabase'
 
 export default function App() {
+  const supabase = useSupabase()
+
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['my-books'],
+    queryFn: async () =>
+      supabase.from('user-books').select('*, book:books(*)').throwOnError(),
+  })
+
+  if (isLoading) return <ActivityIndicator />
+
+  if (error) return <Text>Error: {error.message}</Text>
+
+  console.log(JSON.stringify(data?.data, null, 2))
+
   return (
     <>
       <FlatList
-        data={books}
-        contentContainerClassName='gap-4 bg-green-500'
-        renderItem={({ item }) => <BookListItem book={item} />}
+        data={data?.data || []}
+        contentContainerClassName='gap-4'
+        renderItem={({ item }) => <BookListItem book={item.book} />}
       />
 
       <StatusBar style='auto' />
